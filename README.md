4f966f98a2da52d1c2fde657dea10ba27778a213
Angekarara <mbabange2020@gmail.com>
2023-08-16 12:52:38 +0200

    optional parameters

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # typescript
+
+on exercise optional parameters is a way of asking typescript to allow us passing an empty argument otherwise
+it returns an error.
--- optional-parameters.ts
+++ optional-parameters.ts
@@ -2,6 +2,6 @@ function proclaim(status?: string) {
     console.log(`I'm ${status || 'not ready...'}`);
   }
   
-  proclaim();
+  proclaim();//output is 'I'm not ready...'
   proclaim('ready?');
   proclaim('ready!');
