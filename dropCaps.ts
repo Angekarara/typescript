408f71592c07379672267007508ee96efed19069
Angekarara <mbabange2020@gmail.com>
2023-10-09 18:12:08 +0200

    dropcaps kata

--- dropCaps.ts
+++ dropCaps.ts
@@ -5,7 +5,6 @@ export function dropCap(s: string): string {
   for (const word of words) {
     if (word.length > 2) {
       strings.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
-      //console.log(strings)
     } else {
       strings.push(word);
     }
