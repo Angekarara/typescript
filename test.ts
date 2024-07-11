64fbc95ff90e5466bf47ca952fac3bd013bac7c6
Angekarara <mbabange2020@gmail.com>
2024-07-11 23:10:10 +0200

    new changes

---
+++ test.ts
@@ -0,0 +1,27 @@
+interface circle {
+  kind: 'circle',
+  dimension: 2;
+}
+interface square {
+  kind: square,
+  side: 4;
+}
+
+type Shape = circle | square;
+
+function radius(shape: Shape): shape is circle{
+return shape.kind === 'circle'
+}
+
+function rady(shape: Shape): shape is square{
+    return false
+}
+
+// let shapes: shape = {
+//   kind: "square",
+//   dimension: 1,
+//   kind: "circle",
+//   side: 4,
+// };
+
+//document.addEventListener('timeupdate',() =>console.log('hahahahaha'))
